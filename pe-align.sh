8725a963d7fd9728d4e256f72defdbdaa05abc26
Jose Espina <Jose@SSITNS-AR1LAB1.local>
2024-10-30 10:57:35 -0500

    Updated .sh/.sub files to optimize resource allocation on CHTC

--- pe-align.sh
+++ pe-align.sh
@@ -15,11 +15,11 @@ fastq1=$2
 fastq2=$3
 
 # copy reads1 and reads2 from staging to input directory
-cp -r /staging/groups/roopra_group/jespina/$fastq1 input
-cp -r /staging/groups/roopra_group/jespina/$fastq2 input
+cp /staging/groups/roopra_group/jespina/$fastq1 input
+cp /staging/groups/roopra_group/jespina/$fastq2 input
 
 # copy index tar to index directory
-cp -r /staging/groups/roopra_group/jespina/index/mm10.tar.gz index
+cp /staging/groups/roopra_group/jespina/index/mm10.tar.gz index
 
 # unpack index tar
 cd index
@@ -32,8 +32,8 @@ echo $fastq1 " and " $fastq2
 echo "Aligning " $samplename " (paired-end)"
 
 # run bowtie2
-bowtie2 --end-to-end --very-sensitive --no-mixed --no-discordant --phred33 -I 10 -X 700 -p 8 -x index/mm10 \
--1 input/$fastq1 -2 input/$fastq2 -S ouput/${samplename}_bowtie2.sam 
+bowtie2 --end-to-end --very-sensitive --no-mixed --no-discordant -I 10 -X 700 -p 8 -x index/mm10 \
+-1 input/$fastq1 -2 input/$fastq2 -S output/${samplename}_bowtie2.sam 
 
 # tar output and move to staging
 tar -czvf ${samplename}_bowtie2.tar.gz output/
